cd57a64bab3049302606938b1ec8f2c1e860e85f
Ishan Gaur <ishang@berkeley.edu>
2025-05-22 01:51:04 +0000

    basic repo setup

---
+++ src/oag/constants.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+HOME_FOLDER = Path(__file__).parent.parent.parent
+DATA_FOLDER = HOME_FOLDER / "data"
+DB_FOLDER = HOME_FOLDER / "db"
+OUTPUT_FOLDER = HOME_FOLDER / "output"
